a5fdd07a98bc0f68a2b50f2178c2c1338f565b77
Gopal <gopal.lohar.dev@gmail.com>
2025-06-16 18:42:15 +0530

    add shadcn (with darkmode)

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/hooks/useTheme";
+
 export default function Demo() {
+  const { setTheme } = useTheme();
   return (
-    <div className="flex h-svh w-full items-center justify-center bg-black pb-4 text-4xl text-white">
-      Look at that - It's a Space Station
+    <div className="bg-background text-foreground flex h-svh w-full items-center justify-center gap-4 pb-4 text-4xl">
+      Look at that - It's a Space Station{" "}
+      <Button
+        onClick={() => {
+          setTheme("system");
+        }}
+      >
+        Cdlick Here
+      </Button>
     </div>
   );
 }
