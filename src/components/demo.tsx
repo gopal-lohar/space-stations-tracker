b254cd6e2d1932d36dc739efea4c2edc156d6dcb
Gopal <gopal.lohar.dev@gmail.com>
2025-06-17 11:36:00 +0530

    fix theme

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,17 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/hooks/useTheme";
+import { useState } from "react";
 
 export default function Demo() {
   const { setTheme } = useTheme();
+  const [dark, setDark] = useState(false);
   return (
     <div className="bg-background text-foreground flex h-svh w-full items-center justify-center gap-4 pb-4 text-4xl">
       Look at that - It's a Space Station{" "}
       <Button
         onClick={() => {
-          setTheme("system");
+          if (dark) {
+            setTheme("light");
+          } else {
+            setTheme("dark");
+          }
+          setDark((dark) => !dark);
         }}
       >
-        Cdlick Here
+        Click Here
       </Button>
     </div>
   );
