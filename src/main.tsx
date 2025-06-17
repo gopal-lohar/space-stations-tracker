ca155d88a7e4c19039197d24a18c401b11731134
Gopal <gopal.lohar.dev@gmail.com>
2025-06-17 18:40:44 +0530

    add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import App from "@/App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "@/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
